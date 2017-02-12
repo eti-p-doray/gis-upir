2b098901505b30f4bd749cf6ad4811c37ef8ea95
Etienne Pierre-Doray <etipdoray@gmail.com>
2017-02-11 22:47:10 -0500

    Compute discontinuity count as feature

--- spat/trajectory/features.py
+++ spat/trajectory/features.py
@@ -8,12 +8,14 @@ import shapefile
 from spat.spatial_graph import SpatialGraph
 from spat.utility import *
 
-def extract_length(trajectory, graph):
+def extract_length(trajectory, graph, predicate):
+  length = 0
   for segment in trajectory:
-    if segment['link'] == 0:
-      length += sg.LineString(segment['geometry']).length
-    else:
-      length += segment['bounds'][1] - segment['bounds'][0]
+    if (predicate(segment['link'])):
+      if segment['link'] == None:
+        length += sg.LineString(segment['geometry']).length
+      else:
+        length += segment['bounds'][1][1] - segment['bounds'][0][1]
   return length
 
 def load_discontinuity(file):
@@ -21,72 +23,111 @@ def load_discontinuity(file):
   points = {}
 
   id_field = next(i for i,v in enumerate(sf.fields) if v[0] == "id_link")-1
-  frequence_field = next(i for i,v in enumerate(sf.fields) if v[0] == "frequency")-1
   for s, r in zip(sf.shapes(), sf.records()):
     points[r[id_field]] = {
       'geometry': sg.Point(s.points[0]), 
-      'properties': {'id': r[id_field], 'frequency': r[frequence_field]}}
+      'properties': {'id': r[id_field]}}
   return points
 
-def load_inter_discontinuity(file):
+def load_discontinuity_inter(file):
   sf = shapefile.Reader(file)
   points = {}
 
-  for i, (s, r) in enumerate(zip(sf.shapes(), sf.records())):
-    points[i] = {
-      'geometry': sg.Point(s.points[0]), 
-      'properties': {'id': i}}
+  id_field = next(i for i,v in enumerate(sf.fields) if v[0] == "nb_classes")-1
+  for s, r in zip(sf.shapes(), sf.records()):
+    points[r[id_field]] = {
+      'geometry': sg.Point(s.points[0])}
   return points
 
 def match_discontinuity(points, graph):
   discontinuity_index = {}
   for i, p in points.iteritems():
     geom = p['geometry']
-    nearby = graph.edge_intersection(bb_bounds(geom.x, geom.y, 2, 2));
+    nearby = graph.node_intersection(bb_bounds(geom.x, geom.y, 2, 2));
 
-    for edge in nearby:
-      distance = sg.LineString(graph.way(edge.object)).distance(geom)
-      if distance < 2:
-        if edge.object not in discontinuity_index:
-          discontinuity_index[edge.object] = []
-        discontinuity_index[edge.object].append(i)
+    min_distance = np.inf
+    best_node = None
+    for node in nearby:
+      distance = graph.intersection(node).distance(geom)
+      if distance < min_distance:
+        min_distance = distance
+        best_node = node
+    if best_node not in discontinuity_index:
+      discontinuity_index[best_node] = []
+    discontinuity_index[best_node].append(i)
   return discontinuity_index
 
 def extract_discontinuity(trajectory, discontinuity_index):
   count = 0
-  edge = 0
-  nodes = trajectory['node']
-  for node in nodes:
-    if node[0] != edge and node[0] != 0:
-      if node[0] in discontinuity_index:
-        count += len(discontinuity_index[node[0]])
-    edge = node[0]
 
+  for segment in trajectory:
+    if segment['link'] != None and segment['bounds'][1][0] == True:
+      node = segment['link'][1]
+      if node in discontinuity_index:
+        count += len(discontinuity_index[node])
   return count
 
 
 def extract_features(trajectories, graph):
-  end_of_facility = load_discontinuity("data/discontinuity/end_of_facility");
-  change_of_facility_type = load_discontinuity("data/discontinuity/change_of_facility_type");
-  intersections_disc = load_inter_discontinuity("data/intersections/intersections_on_bike_network_with_change_in_road_type");
+  end_of_facility = match_discontinuity(
+    load_discontinuity("data/discontinuity/end_of_facility"), graph)
+  change_of_facility_type = match_discontinuity(
+    load_discontinuity("data/discontinuity/change_of_facility_type"), graph)
+  intersections_disc = match_discontinuity(
+    load_discontinuity_inter("data/intersections/intersections_on_bike_network_with_change_in_road_type"), graph)
 
-  end_of_facility_idx = match_discontinuity(end_of_facility, graph)
-  change_of_facility_type_idx = match_discontinuity(change_of_facility_type, graph)
-  intersections_disc_idx = match_discontinuity(intersections_disc, graph)
+  def any_link(link):
+    return True
+  def any_cycling_link(link):
+    if link == None:
+      return False
+    return graph.edge(link)['type'] > 10
+  def designated_roadway(link):
+    if link == None:
+      return False
+    return graph.edge(link)['type'] == 11
+  def bike_lane(link):
+    if link == None:
+      return False
+    return graph.edge(link)['type'] == 13
+  def seperate_cycling_link(link):
+    if link == None:
+      return False
+    return graph.edge(link)['type'] in {14, 15, 16}
+  def offroad(link):
+    if link == None:
+      return False
+    return graph.edge(link)['type'] == 17
 
   features = {}
   for i, trajectory in enumerate(trajectories):
     features[i] = {}
-    features[i]['length'] = extract_length(trajectory, graph)
+    features[i]['length'] = extract_length(trajectory, graph, 
+      any_link)
+    features[i]['length_cycling'] = extract_length(trajectory, graph, 
+      any_cycling_link)
+    features[i]['length_designated_roadway'] = extract_length(trajectory, graph, 
+      designated_roadway)
+    features[i]['length_bike_lane'] = extract_length(trajectory, graph, 
+      bike_lane)
+    features[i]['length_seperate_cycling_link'] = extract_length(trajectory, graph, 
+      seperate_cycling_link)
+    features[i]['length_offroad'] = extract_length(trajectory, graph, 
+      offroad)
 
-    features[i]['end_of_facility'] = extract_discontinuity(trajectory, end_of_facility_idx)
-    features[i]['change_of_facility_type'] = extract_discontinuity(trajectory, change_of_facility_type_idx)
-    features[i]['intersections_disc'] = extract_discontinuity(trajectory, intersections_disc_idx)
+    features[i]['end_of_facility'] = extract_discontinuity(
+      trajectory, end_of_facility)
+    features[i]['change_of_facility_type'] = extract_discontinuity(
+      trajectory, change_of_facility_type)
+    features[i]['intersections_disc'] = extract_discontinuity(
+      trajectory, intersections_disc)
+
+    print features[i].values()
   return features
 
 def main(argv):
   inputfile = 'data/bike_path/mm.pickle'
-  facilityfile = 'data/osm/mtl.pickle'
+  facilityfile = 'data/mtl_geobase/mtl.pickle'
   outputfile = 'data/bike_path/features.json'
   try:
     opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile=","facility="])
@@ -108,11 +149,8 @@ def main(argv):
   print 'output file:', outputfile
 
   with open(facilityfile, 'r') as f:
-    facility = pickle.load(f)
-  graph = SpatialGraph()
-  graph.import_osm(facility)
-  graph.compress()
-  graph.build_spatial_edge_index()
+    graph = pickle.load(f)
+  graph.build_spatial_node_index()
 
   with open(inputfile, 'r') as f:
     data = pickle.load(f)
